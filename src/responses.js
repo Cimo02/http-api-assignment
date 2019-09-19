6586e477bdd41a1d74f65eb5765b61381d2167f4
Cimo02 <tjc3255@g.rit.edu>
2019-09-19 19:56:46 -0400

    finished api homework

--- src/responses.js
+++ src/responses.js
@@ -1,102 +1,131 @@
 const respond = (request, response, status, content, type) => {
-    response.writeHead(status, { 'Content-Type': type });
-    response.write(content);
-    response.end();
+  response.writeHead(status, { 'Content-Type': type });
+  response.write(content);
+  response.end();
 };
 
 const success = (request, response, acceptedTypes) => {
-  if (acceptedTypes[0] === 'text/xml'){
-    let responseXML = "<response>";
-    responseXML = `${responseXML} <message>This is a successful response</message>`;
-    responseXML = `${responseXML} </response>`;
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseXML = '<response><message>This is a successful response</message></response>';
     return respond(request, response, 200, responseXML, 'text/xml');
   }
   const responseJSON = {
     message: 'This is a successful response',
   };
-    
-  const responseString = JSON.stringify(responseJSON);
-  respond(request, response, 200, responseString, 'application/json');
+
+  return respond(request, response, 200, JSON.stringify(responseJSON), 'application/json');
 };
-  
+
 const badRequest = (request, response, acceptedTypes, params) => {
-  if (acceptedTypes[0] === 'text/html'){
+  if (acceptedTypes[0] === 'text/xml') {
     let responseXML;
-    if (params === null){
-      responseXML = "<response>" // FINSIH THIS!!!!!!!!!!!!!!!!!!!!!!!
-    }
-  }
-    const responseJSON = {
-        message:'This request has the valid parameters',
-    }
-    
-    if (params === null){
-        responseJSON.id = 'badRequest';
-        responseJSON.message = 'Missing valid query parameter set to true';
-        respond(request, response, 400, responseJSON, 'application/json');
-        return;
+    if (params.valid === 'true') {
+      responseXML = '<response><message>This request has the valid parameters</message></response>';
+      return respond(request, response, 200, responseXML, 'text/xml');
     }
 
-    respond(request, response, 200, responseJSON, 'application/json');
+    responseXML = '<response><message>Missing valid query parameter set to true</message><id>badRequest</id></response>';
+    return respond(request, response, 400, responseXML, 'text/xml');
+  }
+  const responseJSON = {
+    message: 'Missing valid query parameter set to true',
+  };
+
+  if (params.valid === 'true') {
+    responseJSON.message = 'This request has the valid parameters';
+    return respond(request, response, 200, JSON.stringify(responseJSON), 'application/json');
+  }
+
+  responseJSON.id = 'badRequest';
+  return respond(request, response, 400, JSON.stringify(responseJSON), 'application/json');
 };
-  
+
 const notFound = (request, response, acceptedTypes) => {
-    const responseJSON = {
-      message: 'The page you are looking for was not found.',
-      id: 'notFound',
-    };
-  
-    respond(request, response, 404, responseJSON, 'application/json');
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseXML = '<response><message>The page you are looking for was not found.</message><id>notFound</id></response>';
+    return respond(request, response, 404, responseXML, 'text/xml');
+  }
+
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respond(request, response, 404, JSON.stringify(responseJSON), 'application/json');
 };
 
 const unauthorized = (request, response, acceptedTypes, params) => {
-    const responseJSON = {
-      message: 'You have successfully viewed the content',
-    };
-  
-    if (params === null){
-        responseJSON.id = 'unauthorized';
-        responseJSON.message = 'Missing loggedIn query parameter set to yes';
-        respond(request, response, 401, responseJSON, 'application/json');
-        return;
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML;
+    if (params.loggedIn === 'yes') {
+      responseXML = '<response><message>You have successfully viewed the content</message></response>';
+      return respond(request, response, 200, responseXML, 'text/xml');
     }
 
-    respond(request, response, 200, responseJSON, 'application/json');
+    responseXML = '<response><message>Missing loggedIn query parameter set to yes</message><id>unauthorized</id></response>';
+    return respond(request, response, 401, responseXML, 'text/xml');
+  }
+  const responseJSON = {
+    message: 'Missing loggedIn query parameter set to yes',
+  };
+
+  if (params.loggedIn === 'yes') {
+    responseJSON.message = 'You have successfully viewed the content';
+    return respond(request, response, 200, JSON.stringify(responseJSON), 'application/json');
+  }
+
+  responseJSON.id = 'unauthorized';
+  return respond(request, response, 401, JSON.stringify(responseJSON), 'application/json');
 };
 
 const forbidden = (request, response, acceptedTypes) => {
-    const responseJSON = {
-      id: 'forbidden',
-      message: 'You do not have access to this content',
-    };
-  
-    respond(request, response, 403, responseJSON, 'application/json');
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseXML = '<response><message>You do not have access to this content</message><id>forbidden</id></response>';
+    return respond(request, response, 403, responseXML, 'text/xml');
+  }
+
+  const responseJSON = {
+    id: 'forbidden',
+    message: 'You do not have access to this content',
+  };
+
+  return respond(request, response, 403, JSON.stringify(responseJSON), 'application/json');
 };
 
 const internal = (request, response, acceptedTypes) => {
-    const responseJSON = {
-      id: 'internal',
-      message: 'Internal Server Error. Something went wrong.',
-    };
-  
-    respond(request, response, 500, responseJSON, 'application/json');
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseXML = '<response><message>Internal Server Error. Something went wrong.</message><id>internal</id></response>';
+    return respond(request, response, 500, responseXML, 'text/xml');
+  }
+
+  const responseJSON = {
+    id: 'internal',
+    message: 'Internal Server Error. Something went wrong.',
+  };
+
+  return respond(request, response, 500, JSON.stringify(responseJSON), 'application/json');
 };
 
 const notImplemented = (request, response, acceptedTypes) => {
-    const responseJSON = {
-      id: 'notImplemented',
-      message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
-    };
-  
-    respond(request, response, 501, responseJSON, 'application/json');
+  if (acceptedTypes[0] === 'text/xml') {
+    const responseXML = '<response><message>A get request for this page has not been implemented yet. Check again later for updated content.</message><id>notImplemented</id></response>';
+    return respond(request, response, 500, responseXML, 'text/xml');
+  }
+
+  const responseJSON = {
+    id: 'notImplemented',
+    message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
+  };
+
+  return respond(request, response, 501, JSON.stringify(responseJSON), 'application/json');
 };
-  
+
 module.exports = {
-    success,
-    badRequest,
-    notFound,
-    unauthorized,
-    forbidden,
-    internal,
-    notImplemented,
+  success,
+  badRequest,
+  notFound,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
 };
--- src/server.js
+++ src/server.js
@@ -1,45 +1,44 @@
 const http = require('http'); // pull in the http server module
-const url = require('url'); 
+const url = require('url');
+const query = require('querystring');
 
-const responseHandler = require('./responses.js'); 
+const responseHandler = require('./responses.js');
 const htmlHandler = require('./htmlResponses.js');
 
-//set the port. process.env.PORT and NODE_PORT are for servers like heroku
+// set the port. process.env.PORT and NODE_PORT are for servers like heroku
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//key:value object to look up URL routes to specific functions
+// key:value object to look up URL routes to specific functions
 const urlStruct = {
   '/': htmlHandler.getIndex,
   '/style.css': htmlHandler.getCSS,
-  '/success':responseHandler.success,
-  '/badRequest':responseHandler.badRequest,
-  '/notImplemented':responseHandler.notImplemented,
-  '/internal':responseHandler.internal,
-  '/forbidden':responseHandler.forbidden,
-  '/notFound':responseHandler.notFound,
+  '/success': responseHandler.success,
+  '/badRequest': responseHandler.badRequest,
+  '/notImplemented': responseHandler.notImplemented,
+  '/internal': responseHandler.internal,
+  '/forbidden': responseHandler.forbidden,
+  '/notFound': responseHandler.notFound,
+  '/unauthorized': responseHandler.unauthorized,
   index: htmlHandler.getIndex,
 };
 
-//send this function request and pre-filled response objects.
+// send this function request and pre-filled response objects.
 const onRequest = (request, response) => {
-  //parse the url using the url module
-  //This will let us grab any section of the URL by name
+  // parse the url using the url module
   const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
 
-  //grab the 'accept' headers (comma delimited) and split them into an array
+  // grab the 'accept' headers (comma delimited) and split them into an array
   const acceptedTypes = request.headers.accept.split(',');
 
-  //check if the path name (the /name part of the url) matches 
-  //any in our url object. If so call that function. If not, default to index.
   if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
-  } 
-  else {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
+  } else {
     urlStruct.index(request, response, acceptedTypes);
   }
 };
 
-//start HTTP server 
+// start HTTP server
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
